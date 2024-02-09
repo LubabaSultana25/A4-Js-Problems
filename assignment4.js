9ae56c2cee58d021eb056c4fb696706d301baaed
LubabaSultana25 <tahiaanu.cpi@gmail.com>
2024-02-09 22:09:42 +0600

    console add

--- assignment4.js
+++ assignment4.js
@@ -21,3 +21,7 @@ function password(obj) {
     return password;
 }
 
+console.log(deleteInvalids([1, null, undefined, 18, -19, NaN, "12", [1, 2], { ob: "lala" }])); // Output: [ 1, 18, -19 ]
+console.log(deleteInvalids(["1", { num: 2 }, NaN])); // Output: []
+console.log(deleteInvalids([1, 2, -3])); // Output: [ 1, 2, -3 ]
+console.log(deleteInvalids({ num: [1, 2, 3] })); // Output: "Invalid Array"
