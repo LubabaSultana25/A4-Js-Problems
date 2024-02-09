596913cd100c0f9fbd1071a9a50f4a4efc0f71c5
LubabaSultana25 <tahiaanu.cpi@gmail.com>
2024-02-09 22:17:50 +0600

    update some code

--- assignment3.js
+++ assignment3.js
@@ -15,3 +15,9 @@ function deleteInvalids(array) {
     }
     return validNum;
 }
+
+
+console.log(deleteInvalids([1, null, undefined, 18, -19, NaN, "12", [1, 2], { ob: "lala" }])); // Output: [ 1, 18, -19 ]
+console.log(deleteInvalids(["1", { num: 2 }, NaN])); // Output: []
+console.log(deleteInvalids([1, 2, -3])); // Output: [ 1, 2, -3 ]
+console.log(deleteInvalids({ num: [1, 2, 3] })); // Output: "Invalid Array"
--- assignment4.js
+++ assignment4.js
@@ -21,7 +21,10 @@ function password(obj) {
     return password;
 }
 
-console.log(deleteInvalids([1, null, undefined, 18, -19, NaN, "12", [1, 2], { ob: "lala" }])); // Output: [ 1, 18, -19 ]
-console.log(deleteInvalids(["1", { num: 2 }, NaN])); // Output: []
-console.log(deleteInvalids([1, 2, -3])); // Output: [ 1, 2, -3 ]
-console.log(deleteInvalids({ num: [1, 2, 3] })); // Output: "Invalid Array"
+
+// Test cases
+console.log(password({ name: "kolimuddin", birthYear: 1999, siteName: "google" })); // Output: Google#kolimuddin@1999
+console.log(password({ name: "rahat", birthYear: 2002, siteName: "Facebook" })); // Output: Facebook#rahat@2002
+console.log(password({ name: "toky", birthYear: 200, siteName: "Facebook" })); // Output: Invalid
+console.log(password({ name: "maisha", birthYear: 2002 })); // Output: Invalid
+console.log(password("invalid input")); // Output: Invalid
